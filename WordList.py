ce47ca54963d188e3172011c170dedbbbfc775c1
Zifan Chen <oliverchenzifan@gmail.com>
2019-05-27 18:20:50 -0700

    Update WordList.py
    Add stop words filter
    PS: remenber to pip install nltk and download stopwords

--- WordList.py
+++ WordList.py
@@ -1,27 +1,32 @@
+from nltk.corpus import stopwords
+
+STOPWORDS = set(stopwords.words('english'))
+
 def add_element(token:str, d:dict):
-    if d.get(token) == None:
-        d[token] = 1
-    else:
-        d[token] += 1
+	if token not in STOPWORDS:
+	    if d.get(token) == None:
+	        d[token] = 1
+	    else:
+	        d[token] += 1
 
 
-def count_words(content:list[str]): -> dict
+def count_words(content:list) -> dict:
     d = {}
     for line in content:
         
         start_pointer = 0
         end_pointer = 0
-        for l in my_str:
+        for l in line:
             if l.isalnum():
                 end_pointer += 1
-                if end_pointer == len(my_str):
-                    add_element(my_str[start_pointer:].lower(), d)
+                if end_pointer == len(line):
+                    add_element(line[start_pointer:].lower(), d)
             else:
                 if end_pointer - start_pointer == 0:
                     end_pointer += 1
                     start_pointer = end_pointer
                 else:
-                    token = my_str[start_pointer:end_pointer].lower()
+                    token = line[start_pointer:end_pointer].lower()
                     add_element(token, d)
                     end_pointer += 1
                     start_pointer = end_pointer
