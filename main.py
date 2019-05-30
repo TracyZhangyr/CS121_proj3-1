357b9aaccf266755a4c9bdb0c53099d2ae4ef1d9
Zifan Chen <oliverchenzifan@gmail.com>
2019-05-29 22:31:10 -0700

    Add two funcs to write dict and load

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from Document import Document
 import WordList
 from collections import defaultdict
+import json
 
-def get_list_of_document(document_path):
+def get_list_of_document(document_path)->list:
     list_of_Document = []
     with open(document_path) as json_file:
         for line in json_file:
@@ -14,13 +15,24 @@ def get_list_of_document(document_path):
     return list_of_Document
 
 
-def indexing(index_dict:dict,doc_list:["Document"]):
+def indexing(index_dict:dict,doc_list:["Document"])->None:
     total_doc_num = doc_list.length()
     for doc in doc_list:
         WordList.update_index_dict(index_dict,doc)
     WordList.calculate_tfidf(index_dict,total_doc_num)
 
+def write_dict(d:dict)->None:
+	js = json.dumps(d)   
+	file = open('WordList.txt', 'w+')  
+	file.write(js)  
+	file.close()
 
+def load_dict()->dict:
+	file = open('WordList.txt', 'r') 
+	js = file.read()
+	d = json.loads(js)    
+	file.close() 
+	return d
 
 
 if __name__ == "__main__":
