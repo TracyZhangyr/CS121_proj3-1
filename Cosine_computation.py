7a8edebd3f048fd2970710598549eb583e62df32
Yuerong Zhang <yuerongzhang2017@outlook.com>
2019-05-31 01:15:21 -0700

    modify get_user_query

--- Cosine_computation.py
+++ Cosine_computation.py
@@ -3,12 +3,12 @@ from nltk.corpus import stopwords
 STOPWORDS = set(stopwords.words('english'))
 
 class Cosine_computation:
-    def __init__(self, query_list, word_list: dict):
+    def __init__(self, query_list：[], word_list: dict):
         self.query_list = self.eliminate_stop_words(query_list)
         self.word_dict = word_list
         
-    def cosine_scores():
-        for query in query_list:
+    def cosine_scores(self):
+        for query in self.query_list:
             pass 
     
     def eliminate_stop_words(self, query_list):
