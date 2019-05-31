709cc883053b7a7228a5251342a50e3927dbaf1c
Yuerong Zhang <yuerongzhang2017@outlook.com>
2019-05-31 02:39:57 -0700

    add func to generate the report part 2 (see comments)

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def load_dict(file_name:str)->dict:
 	return d
 
 #return ["url","description"]
-def get_url_and_descrip(docID:str)->list:
+def get_url_and_descrip(docID:str)->["url","descrip"]:
     result = []
     path = "WEBPAGES_RAW\\" + docID.replace('/', '\\')
     doc_dict = load_dict(path)
@@ -51,11 +51,36 @@ def get_url_and_descrip(docID:str)->list:
 def generate_top_urls(docIDs:["docID"])->[["url","descrip"]]:
     return [get_url_and_descrip(docID) for docID in docIDs]  
 
+def write_report_part_2(file_name:str,result:[("query",["docID"])]):
+    output = open(file_name,'w+')
+    for pair in result:
+        query = pair[0]
+        docID_list = pair[1]
+        print("Query: {}".format(query),file=output)
+        print("Number of URLs retrieved: {}".format(len(docID_list)),file=output)
+        print("Top 20 URLs:",file=output)
+        if(len(docID_list) >= 20):
+            for docID in docID_list[:20]:
+                url = get_url_and_descrip(docID)[0]
+                print(url,file=output)
+        print("\n",file=output)
+    output.close()
+
+#Need to complete      
+def generate_report_part_2(file_name:str):
+    report_query_list = ["Informatics","Mondego","Irvine",
+                         "artificial intelligence","computer science"]
+    result = []
+    for query in report_query_list:
+        pass    
+    write_report_part_2(file_name, result)
+
 
 def get_user_query(query:str) -> list:
     user_input = query
     query_list = user_input.rstrip().split()
     return query_list
+
 
 if __name__ == "__main__":
     new_list = get_list_of_document("WEBPAGES_RAW\\bookkeeping.json")
@@ -65,7 +90,14 @@ if __name__ == "__main__":
     write_dict(index_dict, "WordList.txt")
     #d = load_dict("WordList.txt")
     #print(len(d.keys()))
-    query_list = get_user_query()
+    
+    #Use for test before the GUI done 
+    input_query = str(input("Please type your query: "))
+    query_list = get_user_query(input_query)
+
+    #Create report.pdf part 2 only (not finished)
+    generate_report_part_2("Report_Part_2.txt")
+    
     
     
     
