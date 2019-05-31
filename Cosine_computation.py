15e4c20009d98808ddf673fedf889dccc5e68c56
gej4 <gej4@uci.edu>
2019-05-31 01:19:51 -0700

    Update Cosine_computation.py

--- Cosine_computation.py
+++ Cosine_computation.py
@@ -1,15 +1,18 @@
 from nltk.corpus import stopwords
+from main import load_dict
+import math
 
 STOPWORDS = set(stopwords.words('english'))
 
 class Cosine_computation:
-    def __init__(self, query_list, word_list: dict):
+    def __init__(self, query_list:[], word_list: dict):
         self.query_list = self.eliminate_stop_words(query_list)
-        self.word_dict = word_list
+        self.word_dict = load_dict("WordList.txt")
         
-    def cosine_scores():
-        for query in query_list:
-            pass 
+    def cosine_scores(self):
+        for query in self.query_list:
+             if query in word_dict.keys():
+                 pass
     
     def eliminate_stop_words(self, query_list):
         new_list = []
@@ -17,3 +20,10 @@ class Cosine_computation:
             if (query not in STOPWORDS):
                 new_list.append(query)
         return new_list
+    
+    def compute_query_frequency(self, input_query: str):
+        frequency = 0
+        for query in self.query_list:
+            if query == input_query:
+                frequency += 1
+        return (1 + math.log10(frequency))
